4ccf259266b5a8a120053e09cf0028701b39faba
alliwang73 <alliwang@berkeley.edu>
2026-09-12 18:27:07 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -329,7 +329,7 @@ export default function App() {
                 textDecoration: "none",
               }}
             >
-              Hire Me
+              About Me
             </a>
           </div>
 
